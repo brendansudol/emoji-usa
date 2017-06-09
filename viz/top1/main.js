7fb6b66035fb3035c8382255f09eb0d1c43f48bb
brendan sudol <brendansudol@gmail.com>
2017-06-08 23:34:34 -0400

    viz iterate

---
+++ viz/top1/main.js
@@ -0,0 +1,61 @@
+const margin = 4
+const cellsWide = 12
+const cellsHigh = 8
+const labelHeight = 12
+const cellSize = 64
+const cellPad = 10
+const cellInner = cellSize - cellPad
+const imgPad = 4
+const imgSize = cellInner - labelHeight - 2 * imgPad
+
+const metric = 'tfidf'
+const width = cellsWide * cellSize + margin
+const height = cellsHigh * cellSize + margin
+
+const svg = d3
+  .select('#viz')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+
+d3.json('../../data/state-stats.json', (error, data) => {
+  const stateStats = {}
+  data.forEach(d => {
+    const stats = d.stats
+    stateStats[d.id] = {
+      cts: stats.top_counts.map(d => d[0]),
+      tfidf: stats.top_tfidf.map(d => d[0])
+    }
+  })
+
+  const state = svg
+    .append('g')
+    .attr('transform', `translate(${margin / 2}, ${margin / 2})`)
+    .selectAll('.state')
+    .data(data)
+    .enter()
+    .append('g')
+    .attr('class', 'state')
+    .attr(
+      'transform',
+      d => `translate(${d.grid.x * cellSize}, ${(d.grid.y - 1) * cellSize})`
+    )
+
+  state
+    .append('text')
+    .attr('x', cellInner / 2)
+    .attr('y', cellInner - 2)
+    .style('text-anchor', 'middle')
+    .text(d => d.name_short)
+
+  state
+    .append('image')
+    .attr('x', labelHeight / 2 + imgPad)
+    .attr('y', imgPad)
+    .attr('width', imgSize)
+    .attr('height', imgSize)
+    .attr('xlink:href', d => {
+      const emoji = stateStats[d.id][metric][0]
+      return `../../img/${emoji.replace(/:/g, '').toLowerCase()}.png`
+    })
+})
